c16f0975b926dbde07e1ab31662b1911c272beeb
ReemeerieLautaro <lreemeerie@mail.austral.edu.ar>
2025-04-10 14:52:02 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,9 +1,10 @@
 def slice_advanced():
+	# Código a implementar utilizando input.
 	txt= input()
 	print(txt [4:14:2])
 
-slice_advanced()
-    # Código a implementar utilizando input.
+
+   
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
