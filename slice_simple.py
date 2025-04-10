769efd606729bb13f9b0174ff8c31d9e22bb6ec6
ReemeerieLautaro <lreemeerie@mail.austral.edu.ar>
2025-04-10 14:25:13 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,5 +1,11 @@
 def slice_simple():
     texto = "Awesome"
+
+	print (texto[0:3])
+	print (texto[2:5])
+	print (texto[0:4]+texto[4:7])
+
+slice_simple ()
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
 
